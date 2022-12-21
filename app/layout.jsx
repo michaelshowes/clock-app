b8391ec2252306fa38867556d5368756e8665932
Michael Showes <michael.showes@gmail.com>
2022-12-21 04:40:56 -0500

    updated globals and added font

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,21 @@
-import Header from '../components/Header'
-import '../styles/main.scss'
+import { Inter } from '@next/font/google';
+import '../styles/main.scss';
+
+const inter = Inter({
+  subsets: ['latin']
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>
-        <Header />
+      <body className={inter.className}>
         {children}
       </body>
     </html>
-  )
+  );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,28 +1,20 @@
-import data from "../data/db.json"; // Data pulled from local json file
-import { users } from "../util/fetchData.js"; // Data pulled from REST API
+
 
 export default function Home() {
   return (
     <>
-      <h2>Sample local json data</h2>
-      {data.users.map((user) => (
-        <div key={user.id}>
-          <div><strong>{user.name}</strong></div>
-          <div>{user.age}</div>
-          <div>{user.email}</div>
-          <br />
-        </div>
-      ))}
+      <h1>Lorem</h1>
+      <h2>Lorem ipsum dolor sit.</h2>
+      <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+      <h4>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero, ad! Alias provident ea inventore.</h4>
+      <h5>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro voluptate tenetur nam impedit. Possimus minima atque et.</h5>
+      <h6>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore perferendis hic voluptate blanditiis atque.</h6>
 
-      <h2>Sample REST API data</h2>
-
-      <div><strong>{users[0].name}</strong></div>
-      <div>{users[0].username}</div>
-      <div>{users[0].email}</div>
-      <div>{users[0].address.street}</div>
-      <div>{users[0].address.suite}</div>
-      <div>{users[0].address.city}, {users[0].address.zipcode}</div>
-      <br />
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam officia quaerat hic velit rem, doloribus quisquam voluptatibus quo voluptas explicabo, quam nulla incidunt, impedit saepe quae id consectetur nihil inventore.
+        Similique modi quo fugit laudantium ratione enim ipsa eveniet pariatur libero, quae voluptates at deleniti quam dolor architecto accusamus corporis! Unde facere tempore explicabo, impedit rem sapiente pariatur laudantium neque?
+        Neque sequi ipsum eveniet ab necessitatibus possimus quo ducimus veritatis incidunt quod vitae totam fugiat recusandae reiciendis modi, sit atque optio aut harum dolore eaque saepe, provident quaerat? Ipsam, nostrum!
+        Quod corporis necessitatibus consequuntur exercitationem consectetur, placeat commodi harum pariatur, dolores ipsa aspernatur ipsum sunt quos ab maxime error soluta impedit incidunt earum! Similique natus, tempore commodi reprehenderit numquam beatae?
+        Provident sint, in voluptates quos sapiente quod non quas eum delectus veritatis voluptatibus corporis et iure velit quaerat perspiciatis quo debitis quibusdam mollitia ab itaque ratione aliquam. Ut, fugit nemo!</div>
     </>
   )
 }
